a974cb1e92b51bda222e3b7a526c79cded2bfcda
UDAY RAMA KOTESWA RAO NALABOTHU <111573702+Nurkrao2002@users.noreply.github.com>
2022-09-28 20:59:19 +0530

    Update 02_keyword.py

--- 02_keyword.py
+++ 02_keyword.py
@@ -8,4 +8,3 @@ Course: Python Programming in 2nd Floor at Ameerpet"""
 rake_nltk_var.extract_keywords_from_text(text)
 keyword_extracted = rake_nltk_var.get_ranked_phrases()
 print(keyword_extracted)
-
